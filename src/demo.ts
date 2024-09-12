1f9eaf55062fa1d00854d1d3cd36050fa0f68dce
david <d.marsalone@gmail.com>
2024-09-12 08:02:20 +0200

    queryString method

--- src/demo.ts
+++ src/demo.ts
@@ -1,6 +1,7 @@
 //import './demo.scss'
 
 import humanFileSize from "./files/humanFileSize";
+import queryString from "./browser/queryString";
 
 console.log("Hello world"   );
 
@@ -8,3 +9,6 @@ console.log("humanFileSize(1000) = ", humanFileSize(1000)   );
 console.log("humanFileSize(1000, 'fr') = ", humanFileSize(1000, 'fr')   );
 console.log("humanFileSize(1000000000) = ", humanFileSize(1000000000)   );
 console.log("humanFileSize(1000000000000) = ", humanFileSize(1000000000000)   );
+
+console.log("Essayez de recharger la page avec ?hello=world dans l'url"   );
+console.log(`queryString("hello") = `, queryString("hello")   );
