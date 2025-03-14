1c26fc0bb526ce629c7d2b7586aefee00ef588c8
david <d.marsalone@gmail.com>
2025-03-14 08:43:50 +0100

    amélioration query string + iframe message sender

--- src/browser/queryString.ts
+++ src/browser/queryString.ts
@@ -3,10 +3,10 @@
  * @param varName
  * @return {string}
  */
-export default function queryString(varName: string) {
+export default function queryString(varName: string): string | null {
   const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop as string),
   });
 // Get the value of "varName" in eg "https://example.com/?some_key=some_value"
-  return  params[varName];
+  return  params.get(varName);
 }
