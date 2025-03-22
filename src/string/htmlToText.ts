2354c37fa49e90b41883820fa2a35d43610f8d95
david <d.marsalone@gmail.com>
2025-03-22 05:58:48 +0100

    html2Text

---
+++ src/string/htmlToText.ts
@@ -0,0 +1,10 @@
+/**
+ * Convertit un texte HTML en texte brut
+ * @param html
+ */
+export function htmlToText(html: string): string {
+    return html
+        .replace(/<[^>]*>/g, '') // supprime toutes les balises HTML
+        .replace(/\s+/g, ' ')    // remplace les multiples espaces ou retours à la ligne par un espace simple
+        .trim();                 // supprime les espaces de début et fin
+}
