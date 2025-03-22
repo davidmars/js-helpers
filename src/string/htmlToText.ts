4221899a40b37028327f7ed5fa39f44cb9e8ede7
david <d.marsalone@gmail.com>
2025-03-22 10:30:56 +0100

    html2Text

--- src/string/htmlToText.ts
+++ src/string/htmlToText.ts
@@ -6,5 +6,5 @@ export function htmlToText(html: string): string {
     return html
         .replace(/<[^>]*>/g, '') // supprime toutes les balises HTML
         .replace(/\s+/g, ' ')    // remplace les multiples espaces ou retours à la ligne par un espace simple
-        .trim();                 // supprime les espaces de début et fin
+        .trim();                 // supprime les espaces de début et fin.
 }
