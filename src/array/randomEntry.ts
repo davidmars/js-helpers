508b44d9c1955eb5c358ec4ff09aa8cd5fb05f5a
david <d.marsalone@gmail.com>
2025-06-27 16:08:44 +0200

    update randomEntry function to use proper types

--- src/array/randomEntry.ts
+++ src/array/randomEntry.ts
@@ -2,6 +2,6 @@
  * Returns a random entry from the items array
  * @param items The source array
  */
-export function randomEntry(items:[]){
+export function randomEntry(items:any[]):any {
     return items[Math.floor(Math.random()*items.length)];
 }
