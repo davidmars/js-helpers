335f3004c21d522aadad5bda274b873ae530a340
david <d.marsalone@gmail.com>
2025-12-30 16:55:53 +0100

    add callback support to copyClipboard function for success handling

--- src/browser/copyClipboard.ts
+++ src/browser/copyClipboard.ts
@@ -1,7 +1,18 @@
 /**
  * Copy text to clipboard
  * @param text
+ * @param cb callback(success)
  */
-export default function copyClipboard(text: string) {
-  navigator.clipboard.writeText(text).then();
+export default function copyClipboard(
+  text: string,
+  cb: (success: boolean) => void = () => { }
+): void {
+  if (!navigator.clipboard?.writeText) {
+    cb(false);
+    return;
+  }
+
+  navigator.clipboard.writeText(text)
+    .then(() => cb(true))
+    .catch(() => cb(false));
 }
