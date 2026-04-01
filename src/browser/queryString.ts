7f6b69ca21a7b98d747b2b49e34bb86d7c5e9d4a
david <d.marsalone@gmail.com>
2026-04-01 09:37:53 +0200

    docs: amélioration de la documentation des paramètres de la fonction queryString

--- src/browser/queryString.ts
+++ src/browser/queryString.ts
@@ -1,12 +1,10 @@
 /**
  * Returns querystring value
- * @param varName
- * @return {string}
+ * @param varName Query string parameter name
+ * @returns Parameter value or null if absent
  */
 export default function queryString(varName: string): string | null {
-  const params = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop as string),
-  });
-// Get the value of "varName" in eg "https://example.com/?some_key=some_value"
-  return  params.get(varName);
+  const params = new URLSearchParams(window.location.search);
+  // Get the value of "varName" in eg "https://example.com/?some_key=some_value"
+  return params.get(varName);
 }
